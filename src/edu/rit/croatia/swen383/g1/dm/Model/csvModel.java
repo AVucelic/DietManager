6cc45c86cb4dc44de2e4f77da32f94669fe41c8b
Ejup Elezi <ee1641@rit.edu>
2024-03-20 13:05:59 +0100

    Implemented the csvmodel abstract class

--- src/edu/rit/croatia/swen383/g1/dm/Model/csvModel.java
+++ src/edu/rit/croatia/swen383/g1/dm/Model/csvModel.java
@@ -1,5 +1,13 @@
 package Model;
 
-public abstract class csvModel {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.FileHandler;
 
+public abstract class csvModel {
+    protected FileHandler fileHandler;
+
+    public abstract ArrayList<Object> read() throws IOException;
+
+    public abstract void write() throws IOException;
 }
