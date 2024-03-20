39f9709efff6c425cd64091bd35542895c78071a
Janko Leskovac <jl8592@rit.edu>
2024-03-20 01:22:10 +0100

    Finished creating a simple GUI for the View.java class

--- src/edu/rit/croatia/swen383/g1/dm/DietManagerRunner.java
+++ src/edu/rit/croatia/swen383/g1/dm/DietManagerRunner.java
@@ -1,5 +1,8 @@
+import View.View;
+import javafx.application.Application;
+
 public class DietManagerRunner {
     public static void main(String[] args) {
-
+        Application.launch(View.class, args);
     }
 }
