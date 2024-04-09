b7976038a88d0600b31e0c766cbbad852745b8a1
Ejup Elezi <ee1641@rit.edu>
2024-04-09 23:54:33 +0200

    Implemented the Exercises class

---
+++ src/edu/rit/croatia/swen383/g1/dm/Model/Exercises.java
@@ -0,0 +1,62 @@
+package Model;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Exercises extends csvModel {
+    ArrayList<Object> exercises;
+
+    public Exercises(FileHandler fh) {
+        super(fh);
+        exercises = new ArrayList<>();
+    }
+
+    @Override
+    public ArrayList<Object> read(String filepath) throws IOException {
+        exercises = new ArrayList<>();
+        BufferedReader br = new BufferedReader(fh.getReader(filepath));
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] attribues = line.split(",");
+            DailyExercise exercise = new DailyExercise(attribues[0], attribues[1], Double.parseDouble(attribues[2]));
+            exercises.add(exercise);
+        }
+        br.close();
+        return exercises;
+    }
+
+    @Override
+    public void write(String filepath, Object item) throws IOException {
+        BufferedWriter bw = new BufferedWriter(fh.getWriter(filepath));
+        bw.newLine();
+        DailyExercise de = (DailyExercise) item;
+        bw.write(de.formatToCSV());
+        bw.flush();
+        bw.close();
+    }
+
+    @Override
+    public ArrayList<Object> getData() {
+        return this.exercises;
+    }
+
+    @Override
+    public void setData(ArrayList<Object> data) {
+        this.exercises = data;
+    }
+
+    @Override
+    public void update(int index, Object item) throws IOException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void remove(Object item) throws IOException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+
+}
